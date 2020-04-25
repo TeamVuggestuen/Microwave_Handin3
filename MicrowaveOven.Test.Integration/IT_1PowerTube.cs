05957daa904f42bc7a2b81bcfab31c0dc238363f
Brian Routhaues Boeg Stjernholm <au170668@uni.au.dk>
2020-04-26 00:44:19 +0200

    Added Test.Integration project to solution Added IT1: powertube

---
+++ MicrowaveOven.Test.Integration/IT_1PowerTube.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MicrowaveOvenClasses.Boundary;
+using MicrowaveOvenClasses.Interfaces;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace MicrowaveOven.Test.Integration
+{
+    [TestFixture]
+    public class IT_1PowerTube
+    {
+        //NB
+            //Remember WHITEBOX
+            //Interval (EET) & exceptions/throws already tested in UT
+            //Top module(powertube) has no return values => testing solely on no throws and correct output
+
+        //System under test
+        private PowerTube powertubeTM; //TM = Top module
+        private IOutput output;
+
+        [SetUp]
+        public void Setup()
+        {
+            output = new Output();
+            powertubeTM = new PowerTube(output);
+        }
+
+        [TestCase(50)]
+        [TestCase(500)]
+        //Legal interval betwen: 1-100
+        public void TurnOn_WasOffCorrectPower_CorrectOutput(int power)
+        {
+            //String should only be output once (received(1)), since second value is out of legal interval
+            powertubeTM.TurnOn(power);
+            output.Received(1).OutputLine(Arg.Is<string>(str => str.Contains($"{power}"))); 
+        }
+
+
+        [TestCase(70)]
+        //Testing for no exceptions thrown with legal interval value
+        public void TurnOn_WasOffCorrectpower_NoThrows(int power)
+        {
+            Assert.That(() => powertubeTM.TurnOn(power), Throws.Nothing);
+        }
+
+
+        [Test]
+        //Testing for no throws on TurnOff()
+        public void TurnOff_WasOff_NoOutput()
+        {
+            Assert.That(() => powertubeTM.TurnOff(), Throws.Nothing);
+        }
+
+        //Or (while also testing output):
+        [Test]
+        public void TurnOff_WasOff_CorrectOutput()
+        {
+            powertubeTM.TurnOff();
+            output.Received(1).OutputLine(Arg.Is<string>(str => str.Contains($"PowerTube turned off")));
+        }
+    }
+}
