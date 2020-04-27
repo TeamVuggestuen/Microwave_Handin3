a8da12237fc5a582f09360e8a5caf09d65e9bbcd
Brian Routhaues Boeg Stjernholm <au170668@uni.au.dk>
2020-04-28 00:37:35 +0200

    Adding IT3 Light

--- MicrowaveOven.Test.Integration/IT3_Light.cs
+++ MicrowaveOven.Test.Integration/IT3_Light.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,49 +20,64 @@ namespace MicrowaveOven.Test.Integration
         //System under test
         private Light light_TM;  //Top module
         private IOutput output;
+        private StringWriter consoleOut;
+
 
         [SetUp]
         public void Setup()
         {
             output = new Output();
             light_TM = new Light(output);
+            consoleOut = new StringWriter();
         }
+
+
+        //------------------ PowerTube & Output ---------------------
+
+        //NB:
+        //(1) Both PowerTube & Output needs to be non substituted
+        //(2) =>  whitebox system perspective: can only test by checking console output
+        //(3) Testing as if string outputline is not known => using: private StringWriter consoleOut;
+
 
         [Test]
         public void TurnOn_IsOff_CorrectOutput()
         {
-            //light_TM.TurnOn();
-            //output.Received().OutputLine("Light is turned on");
-            Assert.That(() => light_TM.TurnOn(), Throws.Nothing);
+            Console.SetOut(consoleOut);
+            light_TM.TurnOn();
+            Assert.That(consoleOut.ToString().Length > 0);
+            //Equals($"Light is turned on"));
         }
 
 
         [Test]
-        public void TurnOn_IsOn_CorrectOutput() // burde denne her ikke give en fejl???
+        public void TurnOn_IsOn_CorrectOutput() 
         {
             light_TM.TurnOn();
-            //light_TM.TurnOn();
-            //output.Received().OutputLine("Light is turned on");
-            Assert.That(() => light_TM.TurnOn(), Throws.Nothing);
+            Console.SetOut(consoleOut);
+            light_TM.TurnOn();
+            Assert.That(consoleOut.ToString().Length == 0);
         }
 
 
         [Test]
-        public void TurnOff_IsOn_CorrectOutput() // burde denne her ikke give en fejl???
+        public void TurnOff_IsOn_CorrectOutput() 
         {
             light_TM.TurnOn();
-            //light_TM.TurnOff();
-            //output.Received().OutputLine("Light is turned off");
-            Assert.That(() => light_TM.TurnOff(), Throws.Nothing);
+            Console.SetOut(consoleOut);
+            light_TM.TurnOff();
+            Assert.That(consoleOut.ToString().Length > 0);
+            //Equals($"Light is turned off"));
         }
 
 
         [Test]
-        public void TurnOff_IsOff_CorrectOutput() // burde denne her ikke give en fejl???
+        public void TurnOff_IsOff_CorrectOutput() 
         {
-            //light_TM.TurnOff();
-            //output.Received().OutputLine("Light is turned off");
-            Assert.That(() => light_TM.TurnOff(), Throws.Nothing);
+            Console.SetOut(consoleOut);
+            light_TM.TurnOff();
+            Assert.That(consoleOut.ToString().Length == 0);
+            //Assert.That(() => light_TM.TurnOff(), Throws.Nothing);
         }
     }
 }
